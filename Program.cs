6dc561fceeb4a31617cb836d3dd0a9225ef4d944
Frederik Carlier <frederik.carlier@keysight.com>
2022-04-08 11:53:23 +0200

    Initial demo project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Buffers.Binary;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+for (int i = 0; i < 1024; i++)
+{
+    var certificate = new X509Certificate2("certificate.pfx", "quamotion", X509KeyStorageFlags.Exportable);
+
+    var cng = (RSACng)certificate.PrivateKey;
+    var exportPolicy = cng.Key.GetProperty("Export Policy", CngPropertyOptions.None);
+    var exportPolicyValue = (CngExportPolicies)BinaryPrimitives.ReadInt32LittleEndian(exportPolicy.GetValue());
+
+    Console.WriteLine($"{i:X4}: Export policy: {exportPolicyValue}");
+}
