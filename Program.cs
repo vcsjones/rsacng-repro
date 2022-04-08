00ca00e771e580e65793c50c2c73fd02a68957eb
Frederik Carlier <frederik.carlier@keysight.com>
2022-04-08 12:01:53 +0200

    More tests

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
 ﻿
+using Org.BouncyCastle.Security;
 using System.Buffers.Binary;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 for (int i = 0; i < 1024; i++)
 {
-    var certificate = new X509Certificate2("certificate.pfx", "quamotion", X509KeyStorageFlags.Exportable);
+    using (var certificate = new X509Certificate2("certificate.pfx", "quamotion", X509KeyStorageFlags.Exportable))
+    {
+        var bouncyCert = DotNetUtilities.FromX509Certificate(certificate);
 
-    var cng = (RSACng)certificate.PrivateKey;
-    var exportPolicy = cng.Key.GetProperty("Export Policy", CngPropertyOptions.None);
-    var exportPolicyValue = (CngExportPolicies)BinaryPrimitives.ReadInt32LittleEndian(exportPolicy.GetValue());
+        using (var cng = (RSACng)certificate.PrivateKey)
+        {
+            var exportPolicy = cng.Key.GetProperty("Export Policy", CngPropertyOptions.None);
+            var exportPolicyValue = (CngExportPolicies)BinaryPrimitives.ReadInt32LittleEndian(exportPolicy.GetValue());
 
-    Console.WriteLine($"{i:X4}: Export policy: {exportPolicyValue}");
+            Console.WriteLine($"{i:X4}: Export policy: {exportPolicyValue}");
+        }
+    }
 }
