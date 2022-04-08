65d5f0c9e9122a0222fca717b9b93dac47685553
Frederik Carlier <frederik.carlier@keysight.com>
2022-04-08 12:21:54 +0200

    Parallelism

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@ using System.Buffers.Binary;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
+var tasks = new List<Task>();
+
 for (int i = 0; i < 1024; i++)
+{
+    tasks.Add(Task.Run(DoWork));
+}
+
+await Task.WhenAll(tasks);
+
+static void DoWork()
 {
     using (var certificate = new X509Certificate2("certificate.pfx", "quamotion", X509KeyStorageFlags.Exportable))
     {
@@ -17,7 +26,7 @@ for (int i = 0; i < 1024; i++)
             var exportPolicy = cng.Key.GetProperty("Export Policy", CngPropertyOptions.None);
             var exportPolicyValue = (CngExportPolicies)BinaryPrimitives.ReadInt32LittleEndian(exportPolicy.GetValue());
 
-            Console.WriteLine($"{i:X4}: Export policy: {exportPolicyValue}");
+            Console.WriteLine($"Export policy: {exportPolicyValue}");
         }
     }
 }
